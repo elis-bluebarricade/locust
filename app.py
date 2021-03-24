b475bdd52f33d0bf889bb68c585844131b56db37
elis-bluebarricade <80972396+elis-bluebarricade@users.noreply.github.com>
2021-03-24 17:59:03 +0100

    master / worker environment variables

--- app.py
+++ app.py
@@ -1,5 +1,30 @@
 import os
 
+# Environment Variables
+#
+# LOCUST_SETTING
+#    set this to "master" or "worker", or leave blank for default (single node, no workers)
+#
+# LOCUST_MASTER_PORT
+#    set this for the worker, leave blank for default 5557
+#
+
+try:
+    LOCUST_SETTING = os.environ['LOCUST_SETTING']
+except KeyError:
+    LOCUST_SETTING = "default"
+
+try:
+    LOCUST_MASTER_PORT = os.environ['LOCUST_MASTER_PORT']
+except KeyError:
+    LOCUST_MASTER_PORT = 5557
+
 os.system("python setup.py build")
 os.system("python setup.py install")
-os.system("locust -f locust_test.py --web-port 8080")
+
+if LOCUST_SETTING.lower() == "master":
+    os.system("locust -f locustfile.py --master --web-port 8080")
+elif LOCUST_SETTING.lower() == "slave" or LOCUST_SETTING.lower() == "worker":
+    os.system("locust -f locustfile.py --worker --master-port={}".format(LOCUST_MASTER_PORT))
+else:
+    os.system("locust -f locustfile.py --web-port 8080")
