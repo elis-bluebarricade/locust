aa52dda486d71d6740ca1ba990053b8740b8e665
elis-bluebarricade <80972396+elis-bluebarricade@users.noreply.github.com>
2021-03-24 18:44:14 +0100

    removed host port option

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ try:
 except KeyError:
     LOCUST_SETTING = "default"
 
-try:
-    LOCUST_MASTER_PORT = os.environ['LOCUST_MASTER_PORT']
-except KeyError:
-    LOCUST_MASTER_PORT = 5557
+#try:
+#    LOCUST_MASTER_PORT = os.environ['LOCUST_MASTER_PORT']
+#except KeyError:
+#    LOCUST_MASTER_PORT = 5557
 
 os.system("python setup.py build")
 os.system("python setup.py install")
@@ -25,6 +25,7 @@ os.system("python setup.py install")
 if LOCUST_SETTING.lower() == "master":
     os.system("locust -f locustfile.py --master --web-port 8080")
 elif LOCUST_SETTING.lower() == "slave" or LOCUST_SETTING.lower() == "worker":
-    os.system("locust -f locustfile.py --worker --master-port={}".format(LOCUST_MASTER_PORT))
+    os.system("locust -f locustfile.py --worker)
+    #os.system("locust -f locustfile.py --worker --master-port={}".format(LOCUST_MASTER_PORT))
 else:
     os.system("locust -f locustfile.py --web-port 8080")
