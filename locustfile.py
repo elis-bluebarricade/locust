555ba909fb27e6555cb32855c1fcf6c58da6ac25
elis-bluebarricade <80972396+elis-bluebarricade@users.noreply.github.com>
2021-04-28 20:34:52 +0200

    support for different API calls

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,7 @@ import os
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-from locust import task, TaskSet, HttpUser
+from locust import task, TaskSet, HttpUser, between
 from locust.contrib.fasthttp import FastHttpUser
 
 import greenlet
@@ -12,62 +12,37 @@ import logging, sys
 import json
 from json import JSONDecodeError
 
+import time
+
 # Environment Variables
 #
 # BB_API_KEY
 #    this needs to be set for this demo locustfile
 #
-# LOCUST_WORKER_ID (NOT USED)
-#    set this on the client, dictates the blockchain address range used 
-#
-
-# https://blockchain-api-blockchain-project.apps.ocp1.ocp.hcl.local/greenbay-network
-
-# try:
-#     LOCUST_WORKER_ID = os.environ['LOCUST_WORKER_ID']
-# except KeyError:
-#     LOCUST_WORKER_ID = 0 # default to first worker address range
 
 try:
     BB_API_KEY = os.environ['BB_API_KEY']
 except KeyError:
     BB_API_KEY = "" # Error, this needs to be set as environment variable
 
-#   index   sender  receiver
-#   0       0       1
-#   1       2       3
-#   2       4       5
-#   3       6       7
-#   4       8       9
-#   5       10      11
-#   ...     ...     ...
-
-#def get_ids(index):
-#    sender = index*2
-#    receiver = index*2+1
-
-# def get_addresses(index):
-#     sender_addr = "worker_{}_addr_{}".format(LOCUST_WORKER_ID, index*2)
-#     receiver_addr = "worker_{}_addr_{}".format(LOCUST_WORKER_ID, index*2+1)
-#     return (sender_addr, receiver_addr)
-
 class BlueBarricadeUser(FastHttpUser):
+    wait_time = between(0, 0)
 
-    # user_id = -1
-    # sender_addr = ""
-    # receiver_addr = ""
-
-    #def on_start(self):
-        # print("user {} started.\n".format(greenlet.getcurrent().minimal_ident))
-        # self.user_id = greenlet.getcurrent().minimal_ident
-        # self.sender_addr, self.receiver_addr = get_addresses(self.user_id)
-        # self.sender_addr = self.user_id
-        # self.receiver_addr = self.user_id
-        # print("\n\n{} : {}, {}".format(self.user_id, self.sender_addr, self.receiver_addr))
+    def on_start(self):
+        print(self.host)
 
     @task
+    def myTask(self):
+        self.api_call = self.host.split('/')[-1]
+        print(self.api_call)
+        if self.api_call == "transferMoneyExperimental":
+            self.transfer()
+        else:
+            self.generic()
+            #time.sleep(1)
+    
     def transfer(self):
-        with self.client.post("/transferMoneyExperimental", verify=False, catch_response=True,
+        with self.client.post("", verify=False, catch_response=True,
             headers={"API_KEY" : BB_API_KEY},
             json={
                 "senderAddress": "experimental_senderAddress",
@@ -99,3 +74,14 @@ class BlueBarricadeUser(FastHttpUser):
                 response.failure("Response Error (JSONDecodeError) : " + response.text)
             except:
                 response.failure("Response Error (exception): " + response.text)
+
+    def generic(self):
+        with self.client.post("", verify=False, catch_response=True,
+            headers={"API_KEY" : BB_API_KEY},
+            json={
+            },
+        ) as response:
+            if response.status_code != 200:
+                response.failure("Response Error: HTTP status: " + str(response.status_code))
+            else:
+                response.success()
